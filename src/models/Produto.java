f526b83476f727790eb7dadca5d32f7e572ea863
Wesly <weslyramalho@yahoo.com.br>
2022-01-17 14:31:12 -0300

    Foi criado as pastas db, models, validadores e a class Produto

---
+++ src/models/Produto.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.Date;
+
+public class Produto {
+	private int id;
+	private String descricao;
+	private double preco;
+	private Date dataValidade;
+	
+	public Produto(int id, String descricao, double preco, Date dataValidade) {
+		
+		this.id = id;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.dataValidade = dataValidade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public Date getDataValidade() {
+		return dataValidade;
+	}
+
+}
