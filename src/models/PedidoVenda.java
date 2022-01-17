6556c9771c8b8c92c6b1065e70a2b80d2e817e9f
Wesly <weslyramalho@yahoo.com.br>
2022-01-17 14:55:25 -0300

    Foi criado a class PedidoVenda

---
+++ src/models/PedidoVenda.java
@@ -0,0 +1,50 @@
+package models;
+
+public class PedidoVenda {
+	
+	private int id;
+	private Cliente cliente;
+	private Estoque estoque;
+	private int quantidade;
+	
+	public PedidoVenda(Cliente cliente, Estoque estoque, int quantidade) {
+		this.cliente = cliente;
+		this.estoque = estoque;
+		this.quantidade = quantidade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Estoque getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Estoque estoque) {
+		this.estoque = estoque;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+
+}
