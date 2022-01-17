a9a65496de78ef4992089b0186ca31a2ca2f81aa
Wesly <weslyramalho@yahoo.com.br>
2022-01-17 15:43:00 -0300

    Foi criado a class UsuariosDB

---
+++ src/db/UsuariosDB.java
@@ -0,0 +1,28 @@
+package db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Usuario;
+
+public class UsuariosDB {
+	private List<Usuario> usuarioList = new ArrayList<>();
+
+	public List<Usuario> getUsuarioList() {
+		return usuarioList;
+	}
+	
+	public Usuario getUsuarioPorID(int id) {
+		return usuarioList.stream()
+				.filter((usuario -> usuario.getId() == id))
+				.findFirst()
+				.get();
+	}
+	
+	public void addNovoUsuario(Usuario usuario) {
+		int id = usuarioList.size() + 1;
+		usuario.setId(id);
+		usuarioList.add(usuario);
+	}
+
+}
